058dfd38a50ed2e2bc40f44870df8d8ad5698cc1
전현성 <skyoun97@gmail.com>
2021-02-15 23:59:16 +0900

    Reservation - fix reserve

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { RoomsService } from '../rooms/rooms.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Room } from '../rooms/entities/room.entity';
 import { Reservation } from './entities/reservation.entity';
