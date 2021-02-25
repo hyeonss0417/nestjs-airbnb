e761263a8032ef7903e22f4831ac3775b65d67cb
전현성 <skyoun97@gmail.com>
2021-02-26 05:37:47 +0900

    Provides dependencies (reservations, rooms, users)

--- src/rooms/rooms.service.spec.ts
+++ src/rooms/rooms.service.spec.ts
@@ -1,4 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Photo } from '../common/entities/photo.entity';
+import { mockRepository } from '../common/mocks/services.mock';
+import { AmenityGroup, AmenityItem } from './entities/amenity.entity';
+import { Room } from './entities/room.entity';
+import { CustomRule, DetailChoice, RuleChoice } from './entities/rule.entity';
 import { RoomsService } from './rooms.service';
 
 describe('RoomsService', () => {
@@ -6,7 +12,37 @@ describe('RoomsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomsService],
+      providers: [
+        RoomsService,
+        {
+          provide: getRepositoryToken(Room),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(RuleChoice),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(CustomRule),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(DetailChoice),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(AmenityItem),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(AmenityGroup),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Photo),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<RoomsService>(RoomsService);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,6 +3,8 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Verification } from '../auth/entities/varification.entity';
 import { MockRepository } from '../common/mocks/interfaces.mock';
 import { mockRepository } from '../common/mocks/services.mock';
+import { MailService } from '../mail/mail.service';
+import { Role } from './entities/role.entity';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -15,8 +17,13 @@ describe('UsersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
+        MailService,
         {
           provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Role),
           useValue: mockRepository(),
         },
         {
