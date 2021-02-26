52bcb41c3fe4ce5b9ab22428e13173c2a19a3966
전현성 <skyoun97@gmail.com>
2021-02-27 05:30:45 +0900

    [Travis-ci] S3 & CodeDeploy Setting

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/app/nestbnb
+REPOSITORY=/home/ubuntu/nestjs-airbnb/dist
 PROJECT_NAME=springboot-webservice
 
 echo "> Build 파일 복사"
 
 cp -r $REPOSITORY/zip/* $REPOSITORY/
 
+CURRENT_=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
-CONTAINER_NAME=nestbnb-api
+CONTAINER_NAME=nestbnb-pm2
 APP_NAME=nestbnb-api
 WORDDIR=/usr/src/app
 APP_PATH=/home/ubuntu/nestjs-airbnb
@@ -19,5 +20,5 @@ git pull
 scp ./dist ${ APP_PATH }
 
 ssh ~~ 
-docker cp ./dist ${CONTAINER_NAME}:${WORDDIR}
+docker cp ./dist $CONTAINER_NAME:$REPOSITORY
 docekr exec -it ${CONTAINER_NAME} pm2 reload ${APP_NAME}
