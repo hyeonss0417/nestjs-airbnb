89c07e6497ad5fe65bc73b811b8a507682abe8af
전현성 <skyoun97@gmail.com>
2021-02-27 08:54:52 +0900

    [Travis-ci] Docker & deploy.sh Setting

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 # 부모 이미지 지정
 FROM node:12
-# yarn 설치
-RUN npm install -g yarn
 # 작업 디렉토리 생성
 WORKDIR /usr/src/app
 # 의존성 설치
-COPY package*.json ./
-RUN yarn
 RUN yarn global add pm2
+RUN pm2 install typescript
+
+# 의존성 라이브러리 설치
+ADD package.json ./
+RUN yarn install
+
 # 소스 추가
-COPY ./dist .
+ADD ecosystem.config.js ./
+ADD .env.* ./
+ADD ./dist ./dist
+
 # 포트 매핑
 EXPOSE 3000
+
 # 실행 명령
-CMD ["pm2-runtime", "start", "ecosystem.config.js", "--env", "production"]
+CMD ["yarn", "pm2:prod"]
--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/nestjs-airbnb/
 CONTAINER_NAME=nestbnb-pm2
-CONTAINER_APP_PATH=/
+CONTAINER_WORKDIR=/usr/src/app
+APP_NAME=nestbnb-api
+IMAGE_NAME=nestbnb
+PORT=3000
+
+cd $REPOSITORY
 
 echo "> Build 파일 복사"
-
 cp -r $REPOSITORY/zip/* $REPOSITORY/
 
-cd $REPOSITORY/
+echo "> 도커 이미지가 존재하는지 확인"
+IMAGE_ID=$(docker images | grep $IMAGE_NAME | awk '{print $3}')
+echo "도커 이미지 id: $CONTAINER_ID"
 
-docker cp ./dist $CONTAINER_NAME:$REPOSITORY
+echo "> 현재 구동중인 컨테이너 확인"
+CONTAINER_ID=$(docker ps | grep $CONTAINER_NAME | awk '{print $1}')
+echo "현재 구동 중인 컨테이너 id: $CONTAINER_ID"
 
+# 컨테이너가 없을경우 컨테이너 재실행
+if [ -z "$CONTAINER_ID" ]; then
+    echo "> 컨테이너 실행"
+    docker rm -f $CONTAINER_NAME
+    docker build -t $IMAGE_NAME:latest .
+    docker rmi $(docker images -f "dangling=true" -q)
+    docker run -d --name $CONTAINER_NAME -p $PORT:$PORT $IMAGE_NAME
+# 컨테이너가 실행중인 경우 무중단 배포
+else
+    echo "> 무중단 배포 시작"
+    docker cp $REPOSITORY/dist $CONTAINER_NAME:$CONTAINER_WORKDIR/dist
+    docker cp $REPOSITORY/package.json $CONTAINER_NAME:$CONTAINER_WORKDIR/package.json
+    docekr exec -it $CONTAINER_NAME yarn install
+    docekr exec -it $CONTAINER_NAME pm2 reload $APP_NAME
+fi
 
-APP_NAME=nestbnb-api
-WORDDIR=/usr/src/app
-APP_PATH=/home/ubuntu/nestjs-airbnb
+echo "> 배포 완료!"
 
-
-
-docekr exec -it ${CONTAINER_NAME} pm2 reload ${APP_NAME}
