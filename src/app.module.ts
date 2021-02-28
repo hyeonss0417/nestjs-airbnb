c6e9b96e90c45239e9c7aa663b2af2af2f3e608a
전현성 <skyoun97@gmail.com>
2021-02-28 23:40:37 +0900

    [Travis-ci] Docker & deploy.sh Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import * as Joi from 'joi';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      envFilePath: '.env' + process.env.NODE_ENV, // === 'dev' ? '.env.dev' : '.env.test',
       //ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
