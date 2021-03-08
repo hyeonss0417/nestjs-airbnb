8dc4ae1abeba2cda33f595eaa440452d96fc3c6d
전현성 <skyoun97@gmail.com>
2021-03-08 13:38:39 +0900

    [Room] Remove unnecessary dependency

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -3,7 +3,6 @@ import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Room } from './entities/room.entity';
-import { Reservation } from '../reservations/entities/reservation.entity';
 import { CustomRule, DetailChoice, RuleChoice } from './entities/rule.entity';
 import { AmenityItem, AmenityGroup } from './entities/amenity.entity';
 import { Photo } from '../common/entities/photo.entity';
